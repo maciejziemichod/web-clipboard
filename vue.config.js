bb04a676f5031e9b671dc3cd04564c4aa3c10689
maciejziemichod <maciej.ziemichod@gmail.com>
2020-11-23 22:23:54 +0100

    page title update

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   publicPath: "/web-clipboard/",
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = "Web Clipboard";
+            return args;
+        })
+}
 };
