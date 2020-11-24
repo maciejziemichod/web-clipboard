d55f918bc8135eb2169c7cb559434a9cd7a7f81d
maciejziemichod <maciej.ziemichod@gmail.com>
2020-11-24 16:16:21 +0100

    vue cli config fix for heroku

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+// gh pages deployment
+
+// module.exports = {
+//   publicPath: "/web-clipboard/",
+//   chainWebpack: config => {
+//     config
+//         .plugin('html')
+//         .tap(args => {
+//             args[0].title = "Web Clipboard";
+//             return args;
+//         })
+// }
+// };
 module.exports = {
-  publicPath: "/web-clipboard/",
+  publicPath: "/",
   chainWebpack: config => {
     config
         .plugin('html')
